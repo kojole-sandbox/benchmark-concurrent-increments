981acdff521c93b1c5d22a14c36fe02e8333a9d8
Yuichi Tanikawa <kojole.jp@gmail.com>
2018-06-16 13:38:55 +0900

    Add run.sh

---
+++ run.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+set -eux
+
+OUT_DIR="$(dirname $0)/out"
+N_INCS=72072000
+
+rm -f $OUT_DIR/*.txt
+
+declare -a kinds=(
+  Atomic
+  Mutex
+  RwLock
+  PlMutex
+  PlRwLock
+  Channel
+  ChannelLock
+  CChannel
+  CChannelLock
+)
+
+for kind in "${kinds[@]}"; do
+  for i in $(seq 1 16); do
+    cargo run --release $kind $i $N_INCS | tee -a "$OUT_DIR/$kind.txt"
+  done
+done
